2fdfd7d8abd45f2339193c1c86277b5efd404e40
Najat Abdella <nabdella00@gmail.com>
2020-11-10 20:38:08 -0500

    Skeleton of Movie Gen

---
+++ practice.py
@@ -0,0 +1,20 @@
+from argparse import ArgumentParser
+import re
+import sys
+from #insert file name# import #class, function(s)#
+
+#Nikky 
+class Movie_Generator: #class
+"""This module reads in a text file with movies and their relevant information. 
+It then returns a list of movies that meets the requirements of the information specified by the user
+Args:
+title (str): the title of the film
+film_type (str): the type of film 
+director (str): the director of the film
+rating (float): the rating of the film 0.0-10.0
+runtime (int): the duration of the film in minutes
+year (int): the year the film was released
+genre (str): the genre of the film 
+Returns:
+a list of movies that meets the requirements of the information put into the generator"""
+fsfd
